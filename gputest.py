ea7d49882ba91bb6d177e3bdbcf5211ee144a110
Tillwege <tillwege@gmail.com>
2021-11-29 22:52:56 +0100

    Testing NN

---
+++ gputest.py
@@ -0,0 +1,7 @@
+import os
+
+os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/bin")
+
+import tensorflow as tf 
+tf.test.is_built_with_cuda()
+tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None)
